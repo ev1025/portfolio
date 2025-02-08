c6bb7b50d4e444a77b0d82c0fa91f0d31bd0343e
ev1025 <eg2874@naver.com>
2025-02-08 13:58:09 +0900

    포커스 수정

--- main.py
+++ main.py
@@ -134,7 +134,8 @@ css = '''
         border: none;  /* 호버 시 테두리 제거 */
     }
     .st-emotion-cache-1mcbg9u.efj1jhq2:active,
-    .st-emotion-cache-1mcbg9u.efj1jhq2:hover {
+    .st-emotion-cache-1mcbg9u.efj1jhq2:hover,
+    .st-emotion-cache-1mcbg9u.efj1jhq2:focus {
         outline: none; /* 호버 시 외곽선 제거 */
         border: none;  /* 호버 시 테두리 제거 */
         color : white;
